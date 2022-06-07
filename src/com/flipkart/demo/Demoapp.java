6b46115c43cea11123992ea85cd5e9435cdfbdb6
Mitali Jain <jain.mitali@BLRETV-02DG8WN.local>
2022-06-07 08:45:32 +0530

    first commit

---
+++ src/com/flipkart/demo/Demoapp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author jain.mitali
+ *
+ */
+public class Demoapp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("this is my first project");
+	}
+
+}
